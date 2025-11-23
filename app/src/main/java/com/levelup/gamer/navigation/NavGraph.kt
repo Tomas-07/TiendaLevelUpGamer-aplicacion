697ff252164defa67da4ee91ea6d775db8764a9f
Tomas <to.castaneda@duocuc.cl>
2025-11-22 22:10:24 -0300

    cambios en NavGraph, catalogo y nueva screen llamada DetalleProducto

--- app/src/main/java/com/levelup/gamer/navigation/NavGraph.kt
+++ app/src/main/java/com/levelup/gamer/navigation/NavGraph.kt
@@ -5,31 +5,53 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.levelup.gamer.screens.*
+import com.levelup.gamer.ui.deps
 
 @Composable
 fun AppNav(nav: NavHostController) {
 
-    NavHost(navController = nav, startDestination = Route.Login.name) {
+    NavHost(
+        navController = nav,
+        startDestination = Route.Splash.name
+    ) {
+
+        composable(Route.Splash.name) {
+            SplashRoute(nav)
+        }
 
         composable(Route.Login.name) {
             LoginScreen(
                 onLogin = {
                     nav.navigate(Route.Catalogo.name) {
                         popUpTo(Route.Login.name) { inclusive = true }
-                        launchSingleTop = true
-                    }},
+                    }
+                },
                 onGoRegister = { nav.navigate(Route.Register.name) }
             )
         }
 
         composable(Route.Register.name) {
-            RegisterScreen(onRegister = { nav.popBackStack() })
+            RegisterScreen(
+                onRegister = { nav.popBackStack() },
+                onGoLogin = { nav.popBackStack() }
+            )
         }
 
         composable(Route.Catalogo.name) {
             CatalogoScreen(
                 onGoCart = { nav.navigate(Route.Carrito.name) },
-                onGoPerfil = { nav.navigate(Route.Perfil.name) }
+                onGoPerfil = { nav.navigate(Route.Perfil.name) },
+                onGoDetail = { codigo ->
+                    nav.navigate("detalle/$codigo")
+                }
+            )
+        }
+
+        composable("detalle/{codigo}") { backStack ->
+            val codigo = backStack.arguments?.getString("codigo") ?: ""
+            DetalleProductoScreen(
+                codigo = codigo,
+                onBack = { nav.popBackStack() }
             )
         }
 
@@ -39,7 +61,6 @@ fun AppNav(nav: NavHostController) {
 
         composable(Route.Perfil.name) {
             PerfilScreen(
-                onBack = { nav.navigate(Route.Catalogo.name) { launchSingleTop = true } },
                 onLogout = {
                     nav.navigate(Route.Login.name) {
                         popUpTo(Route.Catalogo.name) { inclusive = true }
@@ -47,6 +68,5 @@ fun AppNav(nav: NavHostController) {
                 }
             )
         }
-
     }
 }
